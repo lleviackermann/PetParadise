1a53cec48d1715385277a63281826e121cc20b3f
lleviackermann <supritkumar30@gmail.com>
2023-03-22 21:29:18 +0530

    some small changes

--- routes/auth/employee/login.js
+++ routes/auth/employee/login.js
@@ -9,44 +9,44 @@ router.get("/", (req, res) => {
 })
 
 const loginquery = "select * from employeedata where mailId=?"
-const dbpath = path.join(__dirname,"..","..","..","data","index.db")
-const db = new sqlite.Database(dbpath,sqlite.OPEN_READWRITE,err=>{
-    if(err){
+const dbpath = path.join(__dirname, "..", "..", "..", "data", "index.db")
+const db = new sqlite.Database(dbpath, sqlite.OPEN_READWRITE, err => {
+    if (err) {
         console.log(dbpath);
     }
-    else{
+    else {
         console.log("Database Connected");
     }
 })
 
-const checkPassword = (Password,hashedPassword)=>{
-    return bcrypt.compareSync(Password,hashedPassword)
+const checkPassword = (Password, hashedPassword) => {
+    return bcrypt.compareSync(Password, hashedPassword)
 }
-router.post("/post",async function(req,res){
+router.post("/", async function (req, res) {
     let mail = req.body.Email
     let password = req.body.Password
     let matched;
-    await db.all(loginquery,[mail], function(err,rows){
-            if(err){
-                console.log(err.message);
-            }
-            if(rows.length == 0){
-                res.end("Incorrect UserName")
-            }
-          rows.forEach(async (row) => {
+    await db.all(loginquery, [mail], function (err, rows) {
+        if (err) {
+            console.log(err.message);
+        }
+        if (rows.length == 0) {
+            res.end("Incorrect UserName")
+        }
+        rows.forEach(async (row) => {
             console.log(row)
-            matched = await checkPassword(password,row.password)
+            matched = await checkPassword(password, row.password)
             // console.log("matched:"+checkPassword(password,row.password));
             console.log(matched);
-            if(matched == true){
+            if (matched == true) {
                 res.end("Password succesfully  matched")
                 success = true
 
-            }else{
+            } else {
                 res.end("Password is incorrect")
-            }        
+            }
         });
-        })
+    })
 })
 
 
